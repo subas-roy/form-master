041c101ca597a33d77e5664159e39ca27fbd266f
subas-roy <subasroy46@gmail.com>
2024-09-11 05:40:21 +0600

    Handle form with onSubmit and access field data

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
+
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <SimpleForm></SimpleForm>
+    </>
+  )
+}
+
+export default App
